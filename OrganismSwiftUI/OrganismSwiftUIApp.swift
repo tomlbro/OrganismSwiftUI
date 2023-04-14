d31310a5739d6c1bb669b174db32b0cab0dea631
张文涛 <winterarch@foxmail.com>
2023-04-14 14:14:08 +0800

    Initial Commit

---
+++ OrganismSwiftUI/OrganismSwiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  OrganismSwiftUIApp.swift
+//  OrganismSwiftUI
+//
+//  Created by 张文涛 on 2023/4/14.
+//
+
+import SwiftUI
+
+@main
+struct OrganismSwiftUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
